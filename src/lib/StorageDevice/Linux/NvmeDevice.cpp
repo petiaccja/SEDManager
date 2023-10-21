5ce9fd70892aea647a09bc74804961e8247f837e
Péter Kardos <kardospeter1994@hotmail.com>
2023-10-21 20:35:47 +0200

    fix nvme code linux

--- src/lib/StorageDevice/Linux/NvmeDevice.cpp
+++ src/lib/StorageDevice/Linux/NvmeDevice.cpp
@@ -9,6 +9,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <format>
 
 
@@ -28,16 +29,34 @@ NvmeDevice::~NvmeDevice() {
     close(m_file);
 }
 
-NvmeIdentifyController NvmeDevice::IdentifyController() {
+
+std::string_view Trim(std::string_view s) {
+    auto it = std::find_if(s.rbegin(), s.rend(), [](char c) { return !std::isspace(c); });
+    return { std::begin(s), it.base() };
+}
+
+
+NvmeControllerIdentity NvmeDevice::IdentifyController() {
     nvme_admin_cmd command;
     memset(&command, 0, sizeof(command));
-    NvmeIdentifyController data;
+    std::array<std::byte, 4096> data;
     command.opcode = static_cast<uint8_t>(eNvmeOpcode::IDENTIFY_CONTROLLER);
-    command.addr = reinterpret_cast<size_t>(&data);
-    command.data_len = sizeof(data);
+    command.addr = reinterpret_cast<size_t>(data.data());
+    command.data_len = data.size();
     command.cdw10 = 0x0000'0001;
     SendCommand(m_file, command);
-    return data;
+
+    NvmeControllerIdentity identity = {
+        .vendorId = uint16_t(unsigned(data[0]) << 8 | unsigned(data[1])),
+        .subsystemVendorId = uint16_t(unsigned(data[2]) << 8 | unsigned(data[3])),
+        .serialNumber = std::string(Trim({ (const char*)data.data() + 4, (const char*)data.data() + 24 })),
+        .modelNumber = std::string(Trim({ (const char*)data.data() + 24, (const char*)data.data() + 64 })),
+        .firmwareRevision = std::string(Trim({ (const char*)data.data() + 64, (const char*)data.data() + 72 })),
+        .recommendedArbitrationBurst = uint8_t(data[72]),
+        .ieeeOuiIdentifier = uint16_t(unsigned(data[73]) << 16 | unsigned(data[74]) << 8 | unsigned(data[75])),
+    };
+
+    return identity;
 }
 
 void NvmeDevice::SecuritySend(uint8_t securityProtocol,
